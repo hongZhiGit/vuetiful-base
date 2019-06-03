eef191b71ee4a2138fc6a107afe9ee26ace026dd
吴占超 <zhanchao.wu@icloud.com>
2019-06-03 20:33:01 +0800

    i18n

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue';
-import './plugins/axios'
+import './plugins/axios';
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+import i18n from './plugins/i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
