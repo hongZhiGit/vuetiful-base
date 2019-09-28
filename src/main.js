08d1c568585d056c66a613501e3b75921f2b681e
吴占超 <zhanchao.wu@icloud.com>
2019-09-28 11:52:42 +0800

    修正vuetify

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import store from './plugins/store';
 import i18n from './plugins/i18n';
 import { found } from './utils/rules';
 import './routers/guards';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -17,5 +18,6 @@ new Vue({
   router,
   store,
   i18n,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,16 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
-import zhHant from 'vuetify/es5/locale/zh-Hant';
-import '@fortawesome/fontawesome-free/css/all.css'; // Ensure you are using css-loader
+import zhHans from 'vuetify/es5/locale/zh-Hans';
 
-Vue.use(Vuetify, {
-  options: {
-    customProperties: true
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    lang: {
+      locales: { zhHans },
+      current: 'zh-Hans',
+    },
+  icons: {
+    iconfont: 'fa',
   },
-  iconfont: 'fa',
-  lang: {
-    locales: { zhHant },
-    current: 'zh-Hant'
-  }
 });
