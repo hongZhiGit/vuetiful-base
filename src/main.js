2f67cb1911edee1e6463c75957e45d9ba61e7df6
吴占超 <zhanchao.wu@icloud.com>
2019-10-02 20:36:00 +0800

    main update

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue';
 import './plugins/axios';
 import vuetify from './plugins/vuetify';
-import './plugins/vant';
 import App from './App.vue';
 import router from './plugins/router';
 import store from './plugins/store';
 import i18n from './plugins/i18n';
-import { found } from './utils/rules';
 import './routers/guards';
 import './plugins/rules';
 import './plugins/toasted';
 
 Vue.config.productionTip = false;
-
-Vue.prototype.$rules = found;
 
 new Vue({
   router,
